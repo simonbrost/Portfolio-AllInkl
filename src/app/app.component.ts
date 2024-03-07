ac758a0ad30af676985b02d0d9289e0a13233f4c
simonbrost <144888640+simonbrost@users.noreply.github.com>
2024-03-07 15:49:01 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    CommonModule, 
+    HeaderComponent, 
+    AboveTheFoldComponent, 
+    AboutMeComponent, 
+    MySkillsComponent, 
+    PortfolioComponent, 
+    ContactComponent, 
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
