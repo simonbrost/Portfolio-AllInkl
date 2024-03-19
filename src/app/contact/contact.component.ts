615af2b5a8b4842f721183d3863eb392ea9ee1fa
simonbrost <144888640+simonbrost@users.noreply.github.com>
2024-03-19 15:51:37 +0100

    form logic started

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if(ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData)
+    }
+  }
 }
