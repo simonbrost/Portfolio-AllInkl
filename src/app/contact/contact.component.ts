f62fa9d53671eb6fea75cf0b5e351881781df549
simonbrost <144888640+simonbrost@users.noreply.github.com>
2024-04-18 11:14:41 +0200

    php added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
@@ -11,6 +12,8 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class ContactComponent {
 
+  http = inject(HttpClient);
+
   contactData = {
     name: "",
     email: "",
@@ -19,13 +22,35 @@ export class ContactComponent {
 
   borderColorGood = '#4B47FF';
 
-  onSubmit(ngForm: NgForm) {
-    if(ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData)
-    }
-  }
+  mailTest = true;
 
-  warning() {
-    console.log('warning works')
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
   }
 }
