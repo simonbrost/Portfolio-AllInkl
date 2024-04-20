298d91de868e219a50735758f3c6449104ceff82
simonbrost <144888640+simonbrost@users.noreply.github.com>
2024-04-20 19:17:28 +0200

    routing fix 3

--- src/app/imprint/imprint/imprint.component.ts
+++ src/app/imprint/imprint/imprint.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
