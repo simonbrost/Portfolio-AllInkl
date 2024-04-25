f11470dd52bb16bf255a98785f978c26cdd606bb
simonbrost <144888640+simonbrost@users.noreply.github.com>
2024-04-25 19:46:34 +0200

    mailtest true

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -25,7 +25,7 @@ export class ContactComponent {
 
   isAgreed: boolean = false;
   isFormSubmitted: boolean = false;
-  mailTest = false;
+  mailTest = true;
 
   post = {
     endPoint: 'https://simonbrost.netlify.app//sendMail.php',
