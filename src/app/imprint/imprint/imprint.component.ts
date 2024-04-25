a18f60cd475087531a39a030b21ca4f3d6c967b8
simonbrost <144888640+simonbrost@users.noreply.github.com>
2024-04-25 11:43:57 +0200

    imprint and pp styled

--- src/app/imprint/imprint/imprint.component.ts
+++ src/app/imprint/imprint/imprint.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from '../../header/header.component';
+import { FooterComponent } from '../../footer/footer.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [TranslateModule, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [TranslateModule, RouterLink, RouterLinkActive, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, HeaderComponent, FooterComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
